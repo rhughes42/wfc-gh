f7f5d74543e34bba7a3271788cd12f5457a47ae8
Ryan Hughes <rhu@shl.dk>
2021-01-22 16:17:01 +0100

    Initial commit

---
+++ WFC/WFCInfo.cs
@@ -0,0 +1,57 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace WFC
+{
+    public class WFCInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "WFC";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("dca00d70-3656-492b-8742-698326c6bf77");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
