4accf6051329c03be154551d74576751a8b1e3a6
Ryan Hughes <rhu@shl.dk>
2021-01-26 11:15:08 +0100

    Grid setup

---
+++ WFC/Grid.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFC
+{
+    /// <summary>
+    /// Our grid computation class.
+    /// </summary>
+    public class Grid
+    {
+        public Grid()
+        {
+
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Propogate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetGeometry()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
