3c618b66bb704d98d1cf988c441b08ad84395968
Ryan Hughes <rhu@shl.dk>
2021-01-26 11:19:33 +0100

    Implement grid

--- WFC/Grid.cs
+++ WFC/Grid.cs
@@ -11,9 +11,38 @@ namespace WFC
     /// </summary>
     public class Grid
     {
+        public int ExtentsX { get; set; }
+        public int ExtentsY { get; set; }
+        public int SizeX { get; set; }
+        public int SizeY { get; set; }
+        public List<Module> Modules { get; set; }
+
+        /// <summary>
+        /// Default (empty) constructor.
+        /// </summary>
         public Grid()
         {
+            this.ExtentsX = 10;
+            this.ExtentsY = 10;
+            this.SizeX = 6;
+            this.SizeY = 6;
+        }
 
+        /// <summary>
+        /// Standard constructor.
+        /// </summary>
+        /// <param name="extX"></param>
+        /// <param name="extY"></param>
+        /// <param name="sizeX"></param>
+        /// <param name="sizeY"></param>
+        /// <param name="modules"></param>
+        public Grid(int extX, int extY, int sizeX, int sizeY, List<Module> modules)
+        {
+            this.ExtentsX = extX;
+            this.ExtentsY = extY;
+            this.SizeX = sizeX;
+            this.SizeY = sizeY;
+            this.Modules = modules;
         }
 
         public void Initialize()
