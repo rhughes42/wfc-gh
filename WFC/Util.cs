1e12b97a438f105f7276903b41230defa6fdbc1b
Ryan Hughes <rhu@shl.dk>
2021-01-26 12:14:11 +0100

    Added remap

--- WFC/Util.cs
+++ WFC/Util.cs
@@ -11,9 +11,29 @@ namespace WFC
     /// </summary>
     public static class Util
     {
+        /// <summary>
+        /// Convert from Radians to Degrees.
+        /// </summary>
+        /// <param name="radians"></param>
+        /// <returns></returns>
         public static double ToDegrees(double radians)
         {
             return radians * (180 / Math.PI);
+        }
+
+        /// <summary>
+        /// Remap a number from an input range
+        /// to an output range.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="inputMin"></param>
+        /// <param name="inputMax"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <returns></returns>
+        public static double Remap(double input, double inputMin, double inputMax, double min, double max)
+        {
+            return min + (input - inputMin) * (max - min) / (inputMax - inputMin);
         }
     }
 }
